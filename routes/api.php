6abf8c42f0ecbc9f7a69486a5c2716d1de9a3d1f
Aida Real <areal060781@gmail.com>
2018-11-14 16:50:16 -0500

    Create the auth API by using Passport

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 /*Route::get('/users', function () {
     if (rand(1, 10) < 3) {
@@ -25,6 +25,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return factory('App\User', 10)->make();
 });*/
 
-Route::namespace('Api')->group(function () {
+/*Route::namespace('Api')->group(function () {
     Route::get('/users', 'UsersController@index');
+});*/
+
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', 'API\AuthController@logout');
 });
